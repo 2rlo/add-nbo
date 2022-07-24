13107d2b67ec1cfe6ad82fa7d14b84bfead63af9
2rlo <connect.2rl0@gmail.com>
2022-07-25 00:52:26 +0900

    add add-nbo files

---
+++ add-nbo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <netinet/in.h>
+
+int main(int argc, char* argv[]){
+	FILE *fp1, *fp2;
+	fp1 = fopen(argv[1], "rb");
+	fp2 = fopen(argv[2], "rb");
+	
+	uint32_t a, b;
+	fread(&a ,sizeof(uint32_t), 1 ,fp1);
+	fread(&b, sizeof(uint32_t), 1 ,fp2);
+	
+	printf("%d(0x%x) + %d(0x%x) + %d(0x%x)\n", ntohl(a), ntohl(a), ntohl(b), ntohl(b),ntohl(a)+ntohl(b),ntohl(a)+ntohl(b) );
+
+	return 0;
+}
